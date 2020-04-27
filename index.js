d42fcadc35ce9e5d517c1602462e28f86ad90d67
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-04-27 17:58:20 +0200

    build: add ipv6 detection

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict'
 
-const REGEX_LOCALHOST = /^https?:\/\/(localhost|127\.0\.0\.1)/
+const REGEX_LOCALHOST = /^https?:\/\/(localhost|127\.0\.0\.1|::1)/
 
 module.exports = url => REGEX_LOCALHOST.test(url)
