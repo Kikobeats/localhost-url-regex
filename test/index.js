a27248e7b93c951612559f6f9ac469a72e32edb4
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-05-02 16:11:17 +0200

    fix: #1

--- test/index.js
+++ test/index.js
@@ -9,6 +9,7 @@ test('true', t => {
   t.true(isLocalhostUrl('https://localhost:1337'))
   t.true(isLocalhostUrl('https://127.0.0.1:8080'))
   t.true(isLocalhostUrl('http://127.0.0.1:80'))
+  t.true(isLocalhostUrl('http://127.0.1.0:80'))
   t.true(isLocalhostUrl('http://::1:8001'))
   t.true(isLocalhostUrl('https://::1:3001'))
 })
