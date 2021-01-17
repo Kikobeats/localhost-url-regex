c645526aeb60ddee168445f30e6e43b465f30ace
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-01-17 13:38:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,3 @@
 const REGEX_LOCALHOST = /^https?:\/\/(localhost|127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}|::1)/
 
 module.exports = url => REGEX_LOCALHOST.test(url)
-// module.exports.regex = REGEX_LOCALHOST
