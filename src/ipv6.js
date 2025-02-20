d33b344975d3eef0401b6c288c34b12e81b12642
Kiko Beats <josefrancisco.verdu@gmail.com>
2025-02-20 22:30:51 +0100

    feat: improve ipv6 support (#11)

--- src/ipv6.js
+++ src/ipv6.js
@@ -1,11 +1,39 @@
 'use strict'
 
 const IP_RANGES = [
-  // localhost in IPv6
-  /^\[(::1|::)\]$/
+  // Matches IPv4-mapped IPv6 addresses
+  /^::f{4}:([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$/,
+  // Matches IPv4-translated IPv6 addresses
+  /^::f{4}:0.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$/,
+  // Matches IPv4-embedded IPv6 addresses
+  /^64:ff9b::([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$/,
+  // Matches IPv6 addresses in the 100::/64 range
+  /^100::([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4})$/,
+  // Matches IPv6 addresses in the 2001::/32 range
+  /^2001::([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4})$/,
+  // Matches IPv6 addresses in the 2001:20::/28 range
+  /^2001:2[0-9a-fA-F]:([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4})$/,
+  // Matches IPv6 addresses in the 2001:db8::/32 range (documentation prefix)
+  /^2001:db8:([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4})$/,
+  // Matches IPv6 addresses in the 2002::/16 range (6to4)
+  /^2002:([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4}):?([0-9a-fA-F]{0,4})$/,
+  // Matches IPv6 addresses in the fc00::/7 range (ULA)
+  /^f[c-d]([0-9a-fA-F]{2,2}):/i,
+  // Matches IPv6 addresses in the fe80::/10 range (link-local)
+  /^fe[8-9a-bA-B][0-9a-fA-F]:/i,
+  // Matches IPv6 multicast addresses
+  /^ff([0-9a-fA-F]{2,2}):/i,
+  // Matches localhost in IPv6
+  /^::1?$/
 ]
 
-const regex = new RegExp(`^(${IP_RANGES.map(re => re.source).join('|')})$`)
+const regex = () => new RegExp(`^(${IP_RANGES.map(re => re.source).join('|')})$`)
 
-module.exports = regex.test.bind(regex)
-module.exports.regex = regex
+module.exports = hostname => {
+  if (hostname.startsWith('[') && hostname.endsWith(']')) {
+    hostname = hostname.slice(1, -1)
+  }
+  return regex().test(hostname)
+}
+
+module.exports.regex = regex()
--- test/index.js
+++ test/index.js
@@ -7,13 +7,13 @@ const isLocalhost = require('is-local-address')
 const { externalIPs, internalIPs } = require('./cases')
 
 internalIPs.forEach(({ ip }) => {
-  test(`true » ${ip}`, t => {
+  test(`internal » true » ${ip}`, t => {
     t.true(isLocalhost(ip), ip)
   })
 })
 
 externalIPs.forEach(({ ip }) => {
-  test(`false » ${ip}`, t => {
+  test(`external » false » ${ip}`, t => {
     t.false(isLocalhost(ip), ip)
   })
 })
