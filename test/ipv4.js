1050dbf63e9df06f81c7457e16979955ec896fd6
Kiko Beats <josefrancisco.verdu@gmail.com>
2025-02-20 22:12:34 +0100

    feat: improve ipv6 support

--- test/ipv4.js
+++ test/ipv4.js
@@ -7,13 +7,13 @@ const isLocalhost = require('is-local-address/ipv4')
 const { externalIPs, internalIPv4s, internalIPv6s } = require('./cases')
 
 internalIPv4s.forEach(({ ip }) => {
-  test(`true » ${ip}`, t => {
+  test(`internal » true » ${ip}`, t => {
     t.true(isLocalhost(ip), ip)
   })
 })
 
 externalIPs.concat(internalIPv6s).forEach(({ ip }) => {
-  test(`false » ${ip}`, t => {
+  test(`external » false » ${ip}`, t => {
     t.false(isLocalhost(ip), ip)
   })
 })
--- test/ipv6.js
+++ test/ipv6.js
@@ -7,13 +7,13 @@ const isLocalhost = require('is-local-address/ipv6')
 const { externalIpv4s, internalIPv4s, internalIPv6s } = require('./cases')
 
 internalIPv6s.forEach(({ ip }) => {
-  test(`true » ${ip}`, t => {
+  test(`internal » true » ${ip}`, t => {
     t.true(isLocalhost(ip), ip)
   })
 })
 
 externalIpv4s.concat(internalIPv4s).forEach(({ ip, type }) => {
-  test(`false » ${ip}`, t => {
+  test(`external » false » ${ip}`, t => {
     t.false(isLocalhost(ip), ip)
   })
 })
